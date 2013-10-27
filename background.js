809a1f1dc9eeb5cc148bc05a83d886a688dd1858
AiprNick <nicklin10@livemail.tw>
2013-10-27 13:45:32 +0800

    Update background.js

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ function setInfo(tagid,url,description,image)
 	List[dataObj.TagID] = dataObj;
 	chrome.storage.local.set(List);
 }
-function getTagID(key)
+/*function getTagID(key)
 {
 	chrome.storage.local.get(List, function(obj)
 	{
@@ -38,15 +38,29 @@ function getImg(key)
 	{
 		console.log(obj[key].Image);
 	});
-	
-}
+}*/
 
 
 setInfo("Tag2","http://www.google.com.tw","Amazing","/images/2.jpg");
 setInfo("Tag1","http://tw.yahoo.com","wonderful.","/images/1.jpg");
-
+/*
 getTagID("Tag1");
 getUrl("Tag1");
 getDescription("Tag1");
 getImg("Tag1");
 getTagID("Tag2");
+
+*/
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    if (request.method == "getTagID")
+		sendResponse({data: List[request.key].TagID});
+	else if(request.method == "getUrl")
+		sendResponse({data: List[request.key].Url});
+	else if(request.method == "getDescription")
+		sendResponse({data: List[request.key].Description});
+	else if(request.method == "getImg")
+		sendResponse({data: List[request.key].Image});
+    else
+		sendResponse({}); // snub them.
+});
