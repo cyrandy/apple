da3f1b2222566f8ecb13b4136b7be985daec9652
AiprNick <nicklin10@livemail.tw>
2013-10-27 16:03:07 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+
 var List = {};
 
 function setInfo(tagid,url,description,image)
@@ -9,6 +10,7 @@ function setInfo(tagid,url,description,image)
 	dataObj.Image = image;
 	List[dataObj.TagID] = dataObj;
 	chrome.storage.local.set(List);
+	return "save success.";
 }
 /*function getTagID(key)
 {
@@ -41,8 +43,8 @@ function getImg(key)
 }*/
 
 
-setInfo("Tag2","http://www.google.com.tw","Amazing","/images/2.jpg");
-setInfo("Tag1","http://tw.yahoo.com","wonderful.","/images/1.jpg");
+//setInfo(1,"http://www.google.com.tw","Amazing","/images/2.jpg");
+//setInfo(2,"http://tw.yahoo.com","wonderful.","/images/1.jpg");
 /*
 getTagID("Tag1");
 getUrl("Tag1");
@@ -52,15 +54,17 @@ getTagID("Tag2");
 
 */
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    if (request.method == "getTagID")
-		sendResponse({data: List[request.key].TagID});
-	else if(request.method == "getUrl")
-		sendResponse({data: List[request.key].Url});
-	else if(request.method == "getDescription")
-		sendResponse({data: List[request.key].Description});
-	else if(request.method == "getImg")
-		sendResponse({data: List[request.key].Image});
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+	if (msg.method == "setInfo")
+		sendResponse({data: setInfo(msg.key,msg.url,msg.descri,msg.image)});
+	else if (msg.method == "getTagID")
+		sendResponse({data: List[msg.key].TagID});
+	else if(msg.method == "getUrl")
+		sendResponse({data: List[msg.key].Url});
+	else if(msg.method == "getDescription")
+		sendResponse({data: List[msg.key].Description});
+	else if(msg.method == "getImg")
+		sendResponse({data: List[msg.key].Image});
     else
 		sendResponse({}); // snub them.
 });
