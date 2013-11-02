41f3be237b6e86a68de0a68dc966ae603baa7125
kawaiiNatata <ph2.71828@gmail.com>
2013-11-02 18:18:41 +0800

    tester of Shop

--- shop/shop_test.php
+++ shop/shop_test.php
@@ -1,7 +1,15 @@
 <?php
 include 'shop.php';
 
+/*
+public method:
+	findShop(uid) //get all items of the shopper
+	showItems() //show the items fetched
+*/
+
 $shop = new Shop();
 $shop->findShop('Y2655018685');
 $shop->showItems();
+
+
 
